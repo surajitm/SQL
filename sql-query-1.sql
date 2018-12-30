f1ce50b38b1c68f8a52cada9f930796008cd2d5c
surajit mishra <surajitmishra980@gmail.com>
2018-12-30 22:56:42 +0530

    initial commit

---
+++ sql-query-1.sql
@@ -0,0 +1,19 @@
+/*
+Create a database:
+create database <database_name> 
+
+To use a database:
+use <database_name>
+
+
+*/
+
+/*
+create database test_db;
+use test_db;
+*/
+
+show databases;
+
+
+
